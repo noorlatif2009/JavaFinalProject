28c8d9e105ab84b7b7b2ca1ae46863b31f19aa76
noorlatif2009 <noorlatif2109@gmail.com>
2026-05-25 14:03:25 -0700

    Add files via upload

---
+++ MouseInputs.java
@@ -0,0 +1,49 @@
+package Inputs;
+
+import Main.GamePanel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseInputs implements MouseListener,MouseMotionListener {
+    private GamePanel gamePanel;
+
+    public MouseInputs(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void mouseDragged (MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved (MouseEvent e) {
+        gamePanel.setRectPos(e.getX(), e.getY());
+    }
+    
+    @Override
+    public void mouseClicked (MouseEvent e) {
+        gamePanel.changeX(5);
+    }
+
+    @Override
+    public void mousePressed (MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased (MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered (MouseEvent e) {
+
+    }
+
+    public void mouseExited (MouseEvent e) {
+
+    }
+    
+}
